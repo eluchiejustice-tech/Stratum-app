f0b82e6e1788121929353ea12fb5f2f66b8a6eae
eluchiejustice-tech <eluchiejustice@gmail.com>
2026-07-11 02:16:24 +0100

    Wrap MarketplacePage in AuthProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import MarketplacePage from "./pages/MarketplacePage";
+import { AuthProvider } from "./context/AuthContext";
 
 // This file intentionally stays thin. All marketplace logic now lives in
 // pages/MarketplacePage.jsx and the components/services it uses. As more
 // pages are added (ListingDetailPage, ProfilePage, LoginPage) in later
 // stages, real routing will be introduced here.
 export default function App() {
-  return <MarketplacePage />;
+  return (
+    <AuthProvider>
+      <MarketplacePage />
+    </AuthProvider>
+  );
 }
